9165a0f86c5db3d9e063ed63a28fd91a1ebcafa8
onthe6ks <onthe6ks@gmail.com>
2022-03-09 18:07:36 +0900

    init randomi display

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
 import "./styles.css";
+import "./spinkit.css";
+
+import React, { useState } from "react";
 
 export default function App() {
+  const randomList = ["Tokyo", "Chiba", "Saitama"];
+
+  // state
+  const [randomItem, setRandomItem] = useState("where do you want to go ?");
+  const [displayFlag, setDisplayFlag] = useState(true);
+
+  const onButton = () => {
+    const tmpRadom = Math.random() * randomList.length;
+    setDisplayFlag(false);
+    console.log(tmpRadom);
+    setTimeout(() => setDisplayFlag(true), 2000);
+    setRandomItem(randomList[parseInt(tmpRadom, 10)]);
+  };
+
+  const ResultArea = () => {
+    return (
+      <>
+        <h1 className="result">{randomItem}</h1>
+      </>
+    );
+  };
+
+  const Loading = () => {
+    return (
+      <div className="sk-grid">
+        <div className="sk-grid-cube"></div>
+        <div className="sk-grid-cube"></div>
+        <div className="sk-grid-cube"></div>
+        <div className="sk-grid-cube"></div>
+        <div className="sk-grid-cube"></div>
+        <div className="sk-grid-cube"></div>
+        <div className="sk-grid-cube"></div>
+        <div className="sk-grid-cube"></div>
+        <div className="sk-grid-cube"></div>
+      </div>
+    );
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      {displayFlag || <Loading />}
+      {displayFlag && <ResultArea />}
+      <button onClick={onButton}>random</button>
     </div>
   );
 }
